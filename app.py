f9c737d498da3295b1892e6d4f44415c7c320ff2
madheshsathya2004 <106979164+madheshsathya2004@users.noreply.github.com>
2022-07-19 18:36:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import joblib
+model = joblib.load('iris')
+st.title('iris')
+st.title('SPECIES CLASSIFIER')
+ip = st.text_input('Enter the sepal_length,sepal_width,petal_length,petal_widthin this formatx,x,x,x')
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
